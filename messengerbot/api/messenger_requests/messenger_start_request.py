71376a05708f994e155914a52b50373e28bd0ddb
Fedor Buzinov <fedor@prediapp.com>
2017-02-23 03:49:50 +0300

    fix

--- messengerbot/api/messenger_requests/messenger_start_request.py
+++ messengerbot/api/messenger_requests/messenger_start_request.py
@@ -1,12 +1,11 @@
 from future.utils import python_2_unicode_compatible
 from messengerbot.api.event_type import EventType
-from messengerbot.api.user_profile import UserProfile
-from messengerbot.api.viber_requests.viber_request import ViberRequest
+from messengerbot.api.messenger_requests.messenger_request import MessengerRequest
 
 
-class ViberConversationStartedRequest(ViberRequest):
+class MessengerStartRequest(MessengerRequest):
 	def __init__(self):
-		super(ViberConversationStartedRequest, self).__init__(EventType.CONVERSATION_STARTED)
+		super(MessengerStartRequest, self).__init__(EventType.START)
 		self._message_token = None
 		self._type = None
 		self._context = None
@@ -14,16 +13,12 @@ class ViberConversationStartedRequest(ViberRequest):
 		self._api_version = None
 
 	def from_dict(self, request_dict):
-		super(ViberConversationStartedRequest, self).from_dict(request_dict)
+		super(MessengerStartRequest, self).from_dict(request_dict)
 		self._message_token = request_dict['message_token']
 		self._type = request_dict['type']
 		if 'context' in request_dict:
 			self._context = request_dict['context']
-		self._user = UserProfile(request_dict['user']['name'],
-								 request_dict['user']['avatar'],
-								 request_dict['user']['id'],
-								 request_dict['user']['country'],
-								 request_dict['user']['language'])
+		self._user = ''
 		if 'api_version' in request_dict:
 			self._api_version = request_dict['api_version']
 		return self
@@ -50,8 +45,8 @@ class ViberConversationStartedRequest(ViberRequest):
 
 	@python_2_unicode_compatible
 	def __str__(self):
-		return u"ViberConversationStartedRequest [{0}, message_token={1}, type={2}, context{3}, user={4}]"\
-			.format(super(ViberConversationStartedRequest, self).__str__(),
+		return u"MessengerConversationStartedRequest [{0}, message_token={1}, type={2}, context{3}, user={4}]"\
+			.format(super(MessengerStartRequest, self).__str__(),
 					self._message_token,
 					self._type,
 					self._context,
