e8ce7cf5bba7f20dbf96c45c8f8f781251e90f7d
Fedor Buzinov <fedor@prediapp.com>
2017-02-25 16:14:27 +0300

    add quick replies

--- messengerbot/api/messages/quick_replies_message.py
+++ messengerbot/api/messages/quick_replies_message.py
@@ -2,18 +2,22 @@ from messengerbot.api.messages.message import Message
 
 
 class QuikRepliesMessage(Message):
-    def __init__(self, text=None):
+    def __init__(self, text=None, buttons=None):
         self._text = text
+        self._quick_replies = buttons
 
     def to_dict(self):
         message_data = super(QuikRepliesMessage, self).to_dict()
         message_data['message']['text'] = self._text
+        message_data['message']['quick_replies'] = self._quick_replies
         return message_data
 
     def from_dict(self, message_data):
         super(QuikRepliesMessage, self).from_dict(message_data)
         if 'text' in message_data['message']:
             self._text = message_data['message']['text']
+        if 'quick_replies' in message_data['message']:
+            self._quick_replies = message_data['message']['quick_replies']
         return self
 
     def validate(self):
@@ -22,6 +26,10 @@ class QuikRepliesMessage(Message):
     @property
     def text(self):
         return self._text
+
+    @property
+    def quick_replies(self):
+        return self._quick_replies
 
     def __repr__(self):
         from pprint import pformat
