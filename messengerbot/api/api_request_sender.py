8df8e2d8724d52725c4ae1781846abfffffee9c5
Fedor Buzinov <fedor@prediapp.com>
2017-03-12 09:23:16 +0300

    fix

--- messengerbot/api/api_request_sender.py
+++ messengerbot/api/api_request_sender.py
@@ -27,9 +27,11 @@ class ApiRequestSender:
                 data=payload,
                 headers=headers
             )
+            print('params -------> %s' % params)
+            print('payload -------> %s' % payload)
+            print('headers -------> %s' % headers)
             print('response -------> %s' % response)
             response.raise_for_status()
-            print('response.raise_for_status -------> %s' % response)
             return json.loads(response.text)
         except RequestException as e:
             self._logger.error(u"failed to post request to endpoint={0}, with payload={1}. error is: {2}"
