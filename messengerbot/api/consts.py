bd27b1dde2fd03649ec62d4f0c9c01a344c1f7aa
Fedor Buzinov <fedor.buzinov@gmail.com>
2019-02-05 13:03:50 +0300

    Updated api version

--- messengerbot/api/consts.py
+++ messengerbot/api/consts.py
@@ -1,6 +1,6 @@
 from ..version import __version__
 
-DEFAULT_API_VERSION = 2.8
+DEFAULT_API_VERSION = 2.10
 MESSENGER_BOT_API_URL = 'https://graph.facebook.com/v{0}'.format(DEFAULT_API_VERSION)
 MESSENGER_BOT_USER_AGENT = "MessengerBot-Python/" + __version__
 
